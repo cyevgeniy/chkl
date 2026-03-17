04dc871a0aa1ebfed7ccb9ed84b447de78d2561b
Evgeny Chaban <hi@evg.moe>
2026-03-17 22:54:17 +0300

    feat: add typings for custom fields

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,27 +1,36 @@
-export interface Author {
-	name: string
-	url?: string
-	avatar?: string
+interface CustomObject {
+  [k: `_${string}`]:
+    | number
+    | boolean
+    | string
+    | CustomObject
+    | Array<number | string | CustomObject>;
 }
 
-export interface Attachment {
-	url: string
-	mime_type: string
-	alt?: string
+export interface Author extends CustomObject {
+  name: string;
+  url?: string;
+  avatar?: string;
 }
 
-export interface Item {
-	id: string
-	title: string
-	content_text?: string
-	blocking?: boolean
-	attachments?: Attachment[]
+export interface Attachment extends CustomObject {
+  url: string;
+  mime_type: string;
+  alt?: string;
 }
 
-export interface Checklist {
-	title?: string
-	description?: string
-	created_at?: string // https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format
-	authors?: Author[]
-	items: Item[]
+export interface Item extends CustomObject {
+  id: string;
+  title: string;
+  content_text?: string;
+  blocking?: boolean;
+  attachments?: Attachment[];
+}
+
+export interface Checklist extends CustomObject {
+  title?: string;
+  description?: string;
+  created_at?: string; // https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format
+  authors?: Author[];
+  items: Item[];
 }
--- test.ts
+++ test.ts
@@ -1,93 +1,90 @@
-import { Checklist, Item } from "./dist"
+import { Checklist, Item } from "./dist";
 
 /**
  * Extensions fields should start with the `_` symbol
  */
 const minimalExample: Checklist = {
-	items: [
-		{
-			id: '1',
-			title: 'Buy milk',
-		},
-		{
-			id: '2',
-			title: 'Buy bread',
-		},
-		{
-			id: '3',
-			title: 'Buy juice',
-		}
-	]
-}
+  items: [
+    {
+      id: "1",
+      title: "Buy milk",
+    },
+    {
+      id: "2",
+      title: "Buy bread",
+    },
+    {
+      id: "3",
+      title: "Buy juice",
+    },
+  ],
+};
 
 const fullExample: Checklist = {
-	title: 'How to get your first task in our project',
-	description: 'This list describes how you can start working on our project',
-	created_at: '2025-07-17',
-	authors: [
-		{
-			name: 'John Doe',
-			url: 'mailto:johndoe@email.com',
-			avatar: 'https://example.com/avatar/johndoe.png'
-		}
-	],
-	items: [
-		{
-			id: '1',
-			title: 'Register on github',
-			content_text: 'Register on https://github.com',
-			blocking: true,
-		},
-		{
-			id: '2',
-			title: 'Pick a task from the onboarding category',
-			content_text: 'Open github issues board and inspect issues from the "onboarding" category.\nPick the one you want to work on and leave a comment with "I want to work on it" text.',
-			blocking: true,
-		},
-		{
-			id: '3',
-			title: 'Create pull request',
-			content_text: 'Create a pull request. We attached a small video instruction on how to create pull requests',
-			blocking: true,
-			attachments: [
-				{
-					url: 'https://example.com/video/pullrequest.mp4',
-					mime_type: 'video/mp4',
-					alt: 'How to create pull requests'
-				}
-			]
-		},
-	]
-}
+  title: "How to get your first task in our project",
+  description: "This list describes how you can start working on our project",
+  created_at: "2025-07-17",
+  authors: [
+    {
+      name: "John Doe",
+      url: "mailto:johndoe@email.com",
+      avatar: "https://example.com/avatar/johndoe.png",
+    },
+  ],
+  items: [
+    {
+      id: "1",
+      title: "Register on github",
+      content_text: "Register on https://github.com",
+      blocking: true,
+    },
+    {
+      id: "2",
+      title: "Pick a task from the onboarding category",
+      content_text:
+        'Open github issues board and inspect issues from the "onboarding" category.\nPick the one you want to work on and leave a comment with "I want to work on it" text.',
+      blocking: true,
+    },
+    {
+      id: "3",
+      title: "Create pull request",
+      content_text:
+        "Create a pull request. We attached a small video instruction on how to create pull requests",
+      blocking: true,
+      attachments: [
+        {
+          url: "https://example.com/video/pullrequest.mp4",
+          mime_type: "video/mp4",
+          alt: "How to create pull requests",
+        },
+      ],
+    },
+  ],
+};
 
-
-interface Meta {
-	_tag: string
-	_contexts?: string[]
-}
-
-type ExtendedItem = Item & { _meta: Meta}
-
-interface ExtendedChecklist extends Checklist {
-	_test: string,
-	items: ExtendedItem[]
-
-}
-
-const extendedExample: ExtendedChecklist = {
-	_test: '123',
-	items: [
-		{
-			id: '1',
-			title: 'item 1',
-			_meta: {
-				_tag: 'tag1',
-				_contexts: ['ctx1', 'ctx2'],
-			},
-		}
-	]
-}
+const extendedExample: Checklist = {
+  _test: "123",
+  items: [
+    {
+      id: "1",
+      title: "item 1",
+      _meta: {
+        _tag: "tag1",
+        _contexts: ["ctx1", "ctx2"],
+        _customArray: [
+          {
+            _name: "user",
+          },
+        ],
+        _test: {
+          _field: 12,
+          _fieldBool: false,
+          _fieldString: "test",
+        },
+      },
+    },
+  ],
+};
 
 // We can still pass it to other clients:
-const checklist: Checklist = extendedExample
-
+const checklist: Checklist = extendedExample;
