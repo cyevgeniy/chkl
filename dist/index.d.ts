9a101f73e92a14d83eef077b42179fc3918ad8ad
Evgeny Chaban <hi@evg.moe>
2026-03-25 23:52:35 +0300

    feat: export `ChecklistMeta` type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,26 +1,26 @@
-interface CustomObject {
+export interface ChecklistMeta {
   [k: `_${string}`]:
     | number
     | null
     | boolean
     | string
-    | CustomObject
-    | Array<number | string | CustomObject | boolean | null>;
+    | ChecklistMeta
+    | Array<number | string | ChecklistMeta | boolean | null>;
 }
 
-export interface Author extends CustomObject {
+export interface Author extends ChecklistMeta {
   name: string;
   url?: string;
   avatar?: string;
 }
 
-export interface Attachment extends CustomObject {
+export interface Attachment extends ChecklistMeta {
   url: string;
   mime_type: string;
   alt?: string;
 }
 
-export interface Item extends CustomObject {
+export interface Item extends ChecklistMeta {
   id: string;
   title: string;
   content_text?: string;
@@ -28,7 +28,7 @@ export interface Item extends CustomObject {
   attachments?: Attachment[];
 }
 
-export interface Checklist extends CustomObject {
+export interface Checklist extends ChecklistMeta {
   title?: string;
   description?: string;
   created_at?: string; // https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format
